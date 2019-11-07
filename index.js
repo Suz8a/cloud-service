8d6d057a23b4188dbee67f8d298c673c17552d5c
Adriana Ochoa <15171173@itculiacan.com>
2019-11-06 17:04:01 -0700

    props recibed from req

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ admin.initializeApp();
 let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "yourgmailaccount@gmail.com",
-    pass: "yourgmailaccpassword"
+    user: "troquel.mail.sender@gmail.com",
+    pass: "troque_mail_sender"
   }
 });
 
@@ -19,14 +19,21 @@ exports.sendMail = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     // getting dest email by query string
     const dest = req.query.dest;
+    const autor = req.query.autor;
+    const folio = req.query.folio;
+    const cliente = req.query.cliente;
+    const descripcion = req.query.descripcion;
 
     const mailOptions = {
-      from: "Your Account Name <yourgmailaccount@gmail.com>", // Something like: Jane Doe <janedoe@gmail.com>
+      from: autor, // Something like: Jane Doe <janedoe@gmail.com>
       to: dest,
-      subject: "I'M A PICKLE!!!", // email subject
-      html: `<p style="font-size: 16px;">Pickle Riiiiiiiiiiiiiiiick!!</p>
-                <br />
-                <img src="https://images.prod.meredith.com/product/fc8754735c8a9b4aebb786278e7265a5/1538025388228/l/rick-and-morty-pickle-rick-sticker" />
+      subject: "Reporte de Problemas", // email subject
+      html: `<p style="font-size: 16px;">Folio de pedido ${folio}</p>
+                <br/>
+                <span> Cliente: ${cliente} </span>
+                <br/>
+                <span> Descripcion: </span>
+                <p> ${descripcion}</p>
             ` // email content in HTML
     };
 
